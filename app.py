28d35673b0633d05f484d49d6988eba61a71f43f
vijaya657 <pawarvijaya666@gmail.com>
2024-07-09 10:04:54 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-def addition(a,b,c):
-    return a+b+c
-addition(5,6,9)
-
-def subsraction(a,b):
+def addition(a,b):
     return a+b
+addition(5,6)
+
+def subsraction(a,b,c):
+    return a-b-c
 
 def division(a,b):
     return a/b
